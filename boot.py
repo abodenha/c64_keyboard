3a257e7b120a96cb2deab156cd3ba18bac441a9b
Albert Bodenhamer <albert@readers-indigestion.com>
2023-06-30 16:58:37 -0700

    Create boot.py
    Added boot.py to disable drive when not needed

---
+++ boot.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2023 Albert Bodenhamer of Freaking Rad Kreative Network
+#
+# SPDX-License-Identifier: TBD
+""" boot.py for C64 to USB keyboard adapter"""
+import storage
+import usb_midi
+import usb_cdc
+import board
+import digitalio
+
+# Holding restore as the device boots will skip disabling serial and drive.
+restore_pin = digitalio.DigitalInOut(board.D3)
+restore_pin.direction = digitalio.Direction.INPUT
+restore_pin.pull = digitalio.Pull.UP
+
+if restore_pin.value:
+    storage.disable_usb_drive()  # Disable the CIRCUITPY drive connection to USB
+    usb_midi.disable()  # Disable midi
+    usb_cdc.disable()  # Disable both serial devices.
